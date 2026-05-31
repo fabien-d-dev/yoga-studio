18773b4e627b45991496d8a3ed22513012708efb
fabien-d-dev <it.nove@pm.me>
2026-05-31 19:21:02 +0200

    chore(Config): update  main app layout

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,44 @@
+<script setup>
+const route = useRoute()
+</script>
+
 <template>
-  <div>
+  <div class="app-container">
+    <Navbar />
+
     <NuxtRouteAnnouncer />
-    <NuxtPage />
+
+    <main class="main-content">
+      <NuxtPage />
+    </main>
+
+    <AppFooter v-if="route.path !== '/'" />
   </div>
 </template>
+
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #1c1e26;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+.app-container {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.main-content {
+  flex-grow: 1;
+}
+</style>
